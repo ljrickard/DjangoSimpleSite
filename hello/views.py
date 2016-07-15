fc61b5c8b2b2f92b7e526a0e9ae905a9ef3e4792
Richard Dalton <richard@devjoy.com>
2016-07-15 10:24:25 +0100

    Initial Commit

---
+++ hello/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def get_index(request):
+    return render(request, 'index.html')
+
+
+# Create your views here.
+def get_about(request):
+    return render(request, 'about.html')
+
